77c712b62a9df3a0ca89c16f910757b65446e837
Aruna <arunagandhidas@gmail.com>
2022-07-20 21:50:38 +0530

    setup code added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+const bodyParser = require("body-parser");
+
+const routes = require("./routes");
+
+const publicDirPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+app.use(express.static(publicDirPath));
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialPath);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/product', function (req, res) {
   res.send('productPage');
